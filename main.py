2748fd9d92d03537de1e66d7ab1cfa8c5073dbc3
jgrissom <jgrissom91@gmail.com>
2021-07-06 22:42:03 -0500

    led

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from time import sleep
 
 def dot():
     print('.')
+    led_red.on()
+    sleep(.1)
+    led_red.off()
     
 def dash():
     print('-')
+    led_red.on()
+    sleep(.2)
+    led_red.off()
 
 def main():
     while True:
@@ -23,6 +29,8 @@ if __name__ == "__main__":
     try:
         btn_red = Pin(18, Pin.IN, Pin.PULL_UP)
         btn_green = Pin(5, Pin.IN, Pin.PULL_UP)
+        led_red = Pin(26, Pin.OUT)
         main()
     finally:
         print('goodbye')
+        led_red.off()
